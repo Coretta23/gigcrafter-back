ccad9dc8717dafe602e0501c593e2abb2358b0d5
Coretta <machiaecom21@gmail.com>
2023-11-07 17:06:57 +0100

    initialisation

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
+
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+//SERVICES INFO ROUTES
+Route::get('products', [ProductController::class, 'index']);
+Route::post('products', [ProductController::class, 'store']);
+Route::get('products/{id}', [ProductController::class, 'show']);
+Route::get('products/{id}/edit', [ProductController::class, 'edit']);
+Route::put('products/{id}/edit', [ProductController::class, 'update']);
+Route::delete('products/{id}/delete', [ProductController::class, 'destroy']);
+
+//USER ROUTES
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);});
+
